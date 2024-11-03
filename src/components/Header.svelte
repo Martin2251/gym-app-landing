95217d17f7044e8959668d2cf86431cb1e8d71bc
Martin Lowe <martinlowe673@gmail.com>
2024-11-03 14:40:30 +0000

    header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,5 +2,9 @@
 
 </script>
 <header class="flex flex-col relative z-20">
-
+<div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+<a href="/">
+<h1 class="font-semibold">big <span class="text-indigo-400">app</span></h1>
+</a>
+</div>
 </header>
