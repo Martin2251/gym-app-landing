f3dec2dd089baca9311643e32bf3659dc0616809
Martin Lowe <martinlowe673@gmail.com>
2024-11-03 15:03:38 +0000

    h2

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -7,4 +7,7 @@
 </script>
 <SectionWrapper>
     <Header />
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl"></h2>
+    </div>
 </SectionWrapper>
