3d038511f3e1ab823a3b7dbf4fd56cf190fe938d
Martin Lowe <martinlowe673@gmail.com>
2024-11-03 20:08:03 +0000

    product card

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,5 +1,22 @@
 <script>
   import SectionWrapper from "./SectionWrapper.svelte";
 
+  let productFeatures = []
+
 </script>
-<SectionWrapper id="product"></SectionWrapper>
+<SectionWrapper id="product">
+
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
+        <div class="flex flex-col gap-2 ">
+            <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
+                start your lifetime <span class="text-indigo-400">gym streak</span>
+            </p>
+            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">Features for everyone</h3>
+        </div>
+      
+            {#each productFeatures as productFeatures}
+            {/each}
+        
+
+    </div>
+</SectionWrapper>
