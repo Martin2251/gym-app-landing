cfe56f33f60c06900d95d5ed96b8c501566e9a28
Martin Lowe <martinlowe673@gmail.com>
2024-11-05 07:59:12 +0000

    featurelist

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,11 +1,39 @@
 <script>
   import SectionWrapper from "./SectionWrapper.svelte";
 
-  let productFeatures = [{
-    featureList:[],
-    description:"",
-    imgUrl:"",
-  }]
+  let productFeatures = [ {
+            featureList: [
+                "Daily streak counter",
+                "Weights and reps tracker",
+                "Community challenges",
+                "Every workout is new and exciting",
+            ],
+            description:
+                "Swoley moley ensures all users have the information they need to train safely and train effectively.",
+            imgUrl: "assets/selection.png",
+        },
+        {
+            featureList: [
+                "Select your preferences",
+                "Personalize your workouts",
+                "Customize your training",
+                "Guarantees progress on all core exercises",
+            ],
+            description:
+                "Build you streaks and see your progress with our amazing tracking and analytics features.",
+            imgUrl: "assets/training.png",
+        },
+        {
+            featureList: [
+                "Eliminating guesswork",
+                "Save time",
+                "Achieve the performance you seek",
+                "Builds functional strength",
+            ],
+            description:
+                "Enter as much information about yourself as you desire to ensure all workouts are personalized to your every need.",
+            imgUrl: "assets/goal.png",
+        },]
 
 </script>
 <SectionWrapper id="product">
